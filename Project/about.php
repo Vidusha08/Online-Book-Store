6cdeae93576233faaaf76eb4847634c4a9205f2d
Vidusha08 <vidushapherath@gmail.com>
2023-07-08 10:39:10 +0530

    Add all files to Project folder

---
+++ Project/about.php
@@ -0,0 +1,58 @@
+        <section class="page-section">
+        <div class="container">
+            <h2 class="text-center">About Us</h2><br>
+        <!--<p align>PlanetVibes Online Book Store present an enormous assortment of Sri Lankan and unfamiliar distributions covering a variety of various disciplines to meet the assorted necessities of our wide readership that reaches from small children to higher scholastics. we are confided in dealers of a broad assortment of educational books, biographies and magazines through our organization. To keep up with the continuous congruity of supply we maintain our own warehouse and printing facilities.<p><br>
+
+You may be in need of reading because of your childhood habit, all things considered you may not allowed to proceed to surf through the shelves in a bookshop and choose your number one books by reason of your bustling life plan. Appropriately, we are prepared to stay aware of your need through the Internet and convey your top choice book to your entryway step. We are satisfied to give you a more dependable help, with simple installment strategies and a productive help.!-->
+         <!--<CENTER><H2> ABOUT PAGE <H2></CENTER>!-->
+   <br><br><br>
+      <nav> 
+        <a href="#">ABOUT </a>
+        <a href="branch.html">BRANCH </a>
+        <a href="history.HTML">HISTORY</a>
+        <!--<a href="overviw.html">OVERVIW</a>!-->
+        <div class="animation start-home"></div>
+      </nav>
+      <HR>
+
+        <img src=""  id="image" class="image">
+        <script>
+            let image = document.getElementById("image");
+            let images = ['book1.jpg','book2.jpg','book3.jpg','book6.png']
+            setInterval(function(){
+            let random = Math.floor(Math.random() * 4);
+            image.src = images[random];
+            },1100);
+        </script>
+
+       
+        
+
+               
+                    
+                    <h2>  OUR VISION</h2>
+                    <p> Our vision is to become the largest and leading book distributor in Sri Lanka  and to be the bookselling household name in the Sri Lanka Sub Region.</p>
+                
+            
+            
+                    
+                    <h2> OUR MISSON</h2>
+                    <p>Our mission is to provide quality but affordable books for education, entertainment, self-development and self-fulfillment; to all when the need arises by:
+
+                        Providing a wide range of books to satisfy our clients
+                        Exceeding our customers’ expectation in their book requirements
+                        Making our books accessible in the market. 
+                        Acting as agents for both customers and publishers and working for their interest; enriching our client’s life in their quest for educational and entertaining books and advancing the cause of our publishers.</p>
+                </div>
+                
+            
+                   
+                    <h2> OUR VALUES</h2>
+                    <p>Make Way for Books operates and creates positive and sustainable change in the community through the principles of innovation and collaboration, cultivating diverse partnerships, a commitment to high-quality, measurable impact, and constant improvement. Most importantly, we operate with tireless passion, authenticity, exuberance and love for achieving our mission to give children the chance to read and succeed.</p>
+                </div>
+                
+            </div>
+        <!--<?php echo html_entity_decode($_SESSION['system']['about_content'])?>!-->  
+            
+        </div>
+        </section>
